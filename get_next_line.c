67f8d08c7df3939003b728706adb9d67104906dd
Roberto Jornod <rojornod@f1r5s2.codam.nl>
2024-11-29 17:13:14 +0100

    basic file structure

---
+++ get_next_line.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rojornod <rojornod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 11:44:16 by rojornod          #+#    #+#             */
+/*   Updated: 2024/11/29 16:59:22 by rojornod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+char *get_next_line(int fd)
+{
+	char	*buffer;
+	char	*line;
+
+}
+
+char	*extract_line()
+{
+}
+
+char	*update_buffer()
+{
+}
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open(" .txt", O_RDONLY);
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rojornod <rojornod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 11:58:07 by rojornod          #+#    #+#             */
+/*   Updated: 2024/11/29 16:59:11 by rojornod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+
+void	*ft_calloc(size_t num, size_t size);
+void	*ft_memset(void *str, int c, size_t n);
+char 	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *c );
+char	*ft_strchr(const char *str, int search);
+char	*update_buffer(char *buffer);
+char	*ft_free(char *buffer, char *buf);
+char	*extract_line(char *buffer);
+char	*read_from_file(int fd, char *stash);
+
+#endif
